60ced787b5c69caec26c5ac6b73682022cc4361f
PY-GZKY <341796767@qq.com>
2022-03-15 00:13:29 +0800

    Update task.py

--- aiorq/fastapi_server_/app/api/api_v1/task.py
+++ aiorq/fastapi_server_/app/api/api_v1/task.py
@@ -9,4 +9,4 @@ router = APIRouter()
 @router.get("/get_all_functions")
 async def get_all_functions(request: Request):
     results = await request.app.state.redis.all_tasks()
-    return {"results": json.loads(results)}
+    return {"results":results}
