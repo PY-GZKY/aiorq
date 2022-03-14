15704130a0f4700525d4acd63be4fb4c2ffc2274
PY-GZKY <341796767@qq.com>
2022-03-14 23:29:41 +0800

    Update __init__.py

--- aiorq/fastapi_server_/app/api/__init__.py
+++ aiorq/fastapi_server_/app/api/__init__.py
@@ -6,10 +6,10 @@ from aiorq.fastapi_server_.app.api.api_v1.result import router as result_router
 from aiorq.fastapi_server_.app.api.api_v1.task import router as task_router
 from aiorq.fastapi_server_.app.api.api_v1.worker import router as worker_router
 
-router = APIRouter()
+api_v1_router = APIRouter()
 
-router.include_router(index_router, tags=["Index"])
-router.include_router(job_router, prefix="/job", tags=["Job"])
-router.include_router(task_router, prefix="/task", tags=["Task"])
-router.include_router(worker_router, prefix="/worker", tags=["Worker"])
-router.include_router(result_router, prefix="/result", tags=["Result"])
+api_v1_router.include_router(index_router, tags=["Index"])
+api_v1_router.include_router(job_router, prefix="/job", tags=["Job"])
+api_v1_router.include_router(task_router, prefix="/task", tags=["Task"])
+api_v1_router.include_router(worker_router, prefix="/worker", tags=["Worker"])
+api_v1_router.include_router(result_router, prefix="/result", tags=["Result"])
