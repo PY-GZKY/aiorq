1a9e613ad78e40bbaf5f011ea35920882251568e
PY-GZKY <341796767@qq.com>
2022-03-14 18:05:02 +0800

    Update demo.py

--- aiorq/demo.py
+++ aiorq/demo.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import asyncio
-import os
 
-from connections import RedisSettings
 from cron import cron
+from connections import RedisSettings
 
 
 async def say_hello(ctx, name) -> None:
@@ -25,19 +24,24 @@ async def shutdown(ctx):
     print("ending... done")
 
 
-async def run_cron(ctx,time_='2021-11-16 10:26:05'):
-    print(time_)
+
+async def run_regularly(ctx):
+    print('run foo job at 9.12am, 12.12pm and 6.12pm')
 
 
 class WorkerSettings:
     redis_settings = RedisSettings(
-        host=os.getenv("REDIS_HOST", "127.0.0.1"),
-        port=os.getenv("REDIS_PORT", 6379),
-        database=os.getenv("REDIS_DATABASE", 0),
-        password=os.getenv("REDIS_PASSWORD", None)
+        host="127.0.0.1",
+        port=6379,
+        database=0,
+        password=None
     )
 
     functions = [say_hello, say_hi]
+
+    cron_jobs = [
+        cron(run_regularly, hour={17, 12, 18}, minute=12)
+    ]
 
     on_startup = startup
 
@@ -45,6 +49,6 @@ class WorkerSettings:
 
     allow_abort_jobs = True
 
-    worker_name =  "pai"
+    worker_name = "pai"
 
     queue_name = "pai:queue"
