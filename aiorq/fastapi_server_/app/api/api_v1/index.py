9d31107654804a7f580cff9f8cadc62667467222
PY-GZKY <341796767@qq.com>
2022-03-14 23:29:38 +0800

    Update index.py

--- aiorq/fastapi_server_/app/api/api_v1/index.py
+++ aiorq/fastapi_server_/app/api/api_v1/index.py
@@ -1,106 +1,45 @@
-from fastapi import APIRouter, Depends
-from pypika.functions import Date
-from starlette.requests import Request
-from tortoise.functions import Count, Function
+import json
 
-from rearq import ReArq, constants
-from rearq.server import templates
-from rearq.server.depends import get_rearq
-from rearq.server.models import Job, JobResult
+from fastapi import APIRouter
+from starlette.requests import Request
+
+from aiorq.jobs import Job
 
 router = APIRouter()
 
 
-class ToDate(Function):
-    database_func = Date
+@router.get("/index")
+async def index(request: Request):
+    functions = await request.app.state.redis.all_tasks()
+    workers = await request.app.state.redis.all_workers()
+    results = await request.app.state.redis.all_job_results()
+    functions_num = len(json.loads(functions))
+    workers_num = len(workers)
+    results_num = len(results)
+    results = {"functions_num": functions_num, "workers_num": workers_num, "results_num": results_num}
+    return {"results": results}
 
 
-@router.get("/", include_in_schema=False)
-async def index(request: Request, rearq: ReArq = Depends(get_rearq)):
-    task_map = rearq.task_map
-    task_num = len(task_map)
-    workers_info = await rearq.redis.hgetall(constants.WORKER_KEY)
-    worker_num = len(workers_info)
-    run_times = await JobResult.all().count()
-    result = (
-        await Job.all()
-        .annotate(count=Count("id"), date=ToDate("enqueue_time"))
-        .group_by("date", "status")
-        .order_by("date")
-        .values("date", "status", "count")
-    )
-    x_axis = []
-    series = [
-        {
-            "name": "Deferred",
-            "type": "line",
-            "data": [],
-            "stack": "total",
-            "label": {"show": "true"},
-        },
-        {
-            "name": "Queued",
-            "type": "line",
-            "stack": "total",
-            "data": [],
-            "label": {"show": "true"},
-        },
-        {
-            "name": "InProgress",
-            "type": "line",
-            "stack": "total",
-            "data": [],
-            "label": {"show": "true"},
-        },
-        {
-            "name": "Success",
-            "type": "line",
-            "stack": "total",
-            "data": [],
-            "label": {"show": "true"},
-        },
-        {
-            "name": "Failed",
-            "type": "line",
-            "stack": "total",
-            "data": [],
-            "label": {"show": "true"},
-        },
-        {
-            "name": "Expired",
-            "type": "line",
-            "stack": "total",
-            "data": [],
-            "label": {"show": "true"},
-        },
-    ]
-    for item in result:
-        date = str(item.get("date"))
-        if date not in x_axis:
-            x_axis.append(date)
-        count = item.get("count")
-        status = item.get("status")
-        if status == "deferred":
-            series[0]["data"].append(count)
-        elif status == "queued":
-            series[1]["data"].append(count)
-        elif status == "in_progress":
-            series[2]["data"].append(count)
-        elif status == "success":
-            series[3]["data"].append(count)
-        elif status == "failed":
-            series[4]["data"].append(count)
-        elif status == "expired":
-            series[5]["data"].append(count)
-    return templates.TemplateResponse(
-        "dashboard.html",
-        {
-            "request": request,
-            "page_title": "dashboard",
-            "worker_num": worker_num,
-            "task_num": task_num,
-            "run_times": run_times,
-            "x_axis": x_axis,
-            "series": series,
-        },
-    )
+@router.get("/get_health_check")
+async def get_health_check(request: Request, worker_name):
+    result = await request.app.state.redis._get_health_check(worker_name=worker_name)
+    return {"result": json.loads(result)}
+
+
+@router.get("/enqueue_job_")
+async def enqueue_job_(request: Request):
+    job = await request.app.state.redis.enqueue_job('qy_spider_', _queue_name="comment_queue", _job_try=4)
+    job_ = await job.info()
+    return {"job_": job_}
+
+
+@router.get("/queued_jobs")
+async def queued_jobs(request: Request, queue_name="aiorq:queue"):
+    queued_jobs_ = await request.app.state.redis.queued_jobs(queue_name=queue_name)
+    queued_jobs__ = []
+    for queued_job_ in queued_jobs_:
+        state = await Job(job_id=queued_job_.__dict__.get("job_id"), redis=request.app.state.redis,
+                          _queue_name=queue_name).status()
+        queued_job_.__dict__.update({"state": state})
+        queued_jobs__.append(queued_job_)
+    return {"queued_jobs": queued_jobs__}
