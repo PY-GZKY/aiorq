9fba255c0c311050bba883b56a81b25175db35ee
PY-GZKY <341796767@qq.com>
2022-03-14 23:10:24 +0800

    Update __init__.py

--- aiorq/fastapi_server_/app/config/__init__.py
+++ aiorq/fastapi_server_/app/config/__init__.py
@@ -1,9 +1,2 @@
 # -*- coding: utf-8 -*-
-
-import os
-
-env = os.getenv("PRODUCTION", "")
-if env:
-    pass
-else:
-    from .development_config import settings
+from .development_config import settings
